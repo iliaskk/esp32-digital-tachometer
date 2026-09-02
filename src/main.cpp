0eaf0a8ebf28fa9327a0502d18d614e28b6d1162
Ilias <hlias.kontaktsis@gmail.com>
2026-09-02 05:04:12 +0300

    refactor: encapsulate tachometer state into C++ class and secure ISR shared memory
    - Replaced procedural global struct with encapsulated tachometer class
    - Enforced volatile access on ISR-shared variables (lastSparkTime, timeBetweenSparks)
    - Handled micros() 32-bit rollover edge case and engine timeout
    - Cleaned up build flags and compiler warnings (-Wall, -Wextra)

--- src/main.cpp
+++ src/main.cpp
@@ -16,72 +16,86 @@ const uint32_t maxEngineRpm = 9500;
 const uint32_t debounceDelay = microsPerMinute / maxEngineRpm;
 
 // Data Encapsulation (Το "κουτί" με τις μεταβλητές που αλλάζουν)
-struct EngineData
+class tachometer
 {
-    volatile uint32_t lastSparkTime;
-    volatile uint32_t timeBetweenSparks;
+
+private: // Εδώ μπαίνουν οι μεταβλητές που δεν θέλουμε να πειράζει η loop()
     uint32_t currentRpm;
     uint32_t lastRpmState;
+
+public:                                  // Οι μεταβλητές που πρέπει αναγκαστικά να βλέπει το ISR.
+    volatile uint32_t timeBetweenSparks; // αλλάζει τιμή μέσα στο (ISR)
+    volatile uint32_t lastSparkTime;     // αλλάζει τιμή μέσα στο (ISR)
     uint32_t lastDisplayTime;
+
+    //  Constructor
+    tachometer()
+    {
+        lastSparkTime = 0;
+        timeBetweenSparks = 0;
+        currentRpm = 0;
+        lastDisplayTime = 0;
+        lastRpmState = impossibleInitialRpm;
+    }
+
+    // Οι "Λειτουργίες" (API)
+    void(calculateRpm)()
+    {
+        // we use local variables so we can run calculateRpm() and then pass the data to the engine struct to achieve State Management
+        uint32_t localLastSparkTime = 0;
+        uint32_t localTimeBetweenSparks = 0;
+        uint32_t localCurrentRpm = 0;
+        uint32_t localCurrentTime = micros();
+
+        noInterrupts(); // software interrupt
+        localLastSparkTime = lastSparkTime;
+        localTimeBetweenSparks = timeBetweenSparks;
+        interrupts();
+
+        if ((localCurrentTime - localLastSparkTime > engineTimeoutLimit) || (localTimeBetweenSparks == 0))
+        {
+
+            localCurrentRpm = 0; // engine stopped
+
+            noInterrupts();
+            lastSparkTime = localCurrentTime; // Prevents micros() 71-min rollover bug
+            interrupts();
+        }
+        else
+        {
+            localCurrentRpm = microsPerMinute / localTimeBetweenSparks;
+        }
+
+        currentRpm = localCurrentRpm;
+    }
+
+    // Display UI; Change-Detection State for Anti-Flickering
+    void(updateDisplay)()
+    {
+        if (currentRpm != lastRpmState) // if rpm has changed, update the display
+        {
+            tft.setCursor(10, 40);
+            tft.print("RPM: ");
+            tft.print(currentRpm);
+            tft.print("   "); // works like a "smart eraser"
+            Serial.printf("RPM: %" PRIu32 "\n", currentRpm);
+            lastRpmState = currentRpm;
+        }
+    }
 };
 
-// Αρχικοποίηση του struct με τις αρχικές τιμές
-EngineData engine = {0, 0, 0, impossibleInitialRpm, 0};
+tachometer tacho; // object of the tachometer class
 
 // ISR Background
 void IRAM_ATTR recordPulse()
 {
     uint32_t localIsrTime = micros();
-    uint32_t localSparkNew = localIsrTime - engine.lastSparkTime;
+    uint32_t localSparkNew = localIsrTime - tacho.lastSparkTime;
 
     if (localSparkNew > debounceDelay)
     {
-        engine.timeBetweenSparks = localSparkNew;
-        engine.lastSparkTime = localIsrTime;
-    }
-}
-
-void calculateRpm()
-{
-    // we use local variables so we can run calculateRpm() and then pass the data to the engine struct to achieve State Management
-    uint32_t localLastSparkTime = 0;
-    uint32_t localTimeBetweenSparks = 0;
-    uint32_t localCurrentRpm = 0;
-    uint32_t localCurrentTime = micros();
-
-    noInterrupts();
-    localLastSparkTime = engine.lastSparkTime;
-    localTimeBetweenSparks = engine.timeBetweenSparks;
-    interrupts();
-
-    if ((localCurrentTime - localLastSparkTime > engineTimeoutLimit) || (localTimeBetweenSparks == 0))
-    {
-
-        localCurrentRpm = 0; // engine stopped
-
-        noInterrupts();
-        engine.lastSparkTime = localCurrentTime; // Prevents micros() 71-min rollover bug
-        interrupts();
-    }
-    else
-    {
-        localCurrentRpm = microsPerMinute / localTimeBetweenSparks;
-    }
-
-    engine.currentRpm = localCurrentRpm;
-}
-
-// Display UI; Change-Detection State for Anti-flickering
-void updateDisplay()
-{
-    if (engine.currentRpm != engine.lastRpmState) // if rpm has changed, update the display
-    {
-        tft.setCursor(10, 40);
-        tft.print("RPM: ");
-        tft.print(engine.currentRpm);
-        tft.print("   "); // works like a "smart eraser"
-        Serial.printf("RPM: %" PRIu32 "\n", engine.currentRpm);
-        engine.lastRpmState = engine.currentRpm;
+        tacho.timeBetweenSparks = localSparkNew;
+        tacho.lastSparkTime = localIsrTime;
     }
 }
 
@@ -100,16 +114,16 @@ void setup()
 
     pinMode(sparkPin, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(sparkPin), recordPulse, RISING); // Σύνδεσε έναν συναγερμό στο Pin 25. Μόλις δεις την τάση να ανεβαίνει (σπινθήρας), τρέξε τη συνάρτηση recordPulse για να αυξήσεις τον μετρητή
-} // attachInterrupt; an alarm
+} // attachInterrupt; an alarm/HARDWARE interrupt
 
 void loop()
 {
     uint32_t localCurrentLoopTime = millis(); // Δεν βάζουμε ΠΟΤΕ την ίδια μεταβλητή να διαβάζει micros() (εκατομμυριοστά) και μετά από λίγο millis()
 
-    if (localCurrentLoopTime - engine.lastDisplayTime >= displayRefreshRate)
+    if (localCurrentLoopTime - tacho.lastDisplayTime >= displayRefreshRate)
     {
-        calculateRpm();
-        updateDisplay();
-        engine.lastDisplayTime = localCurrentLoopTime; // Αποθήκευση χρόνου τελευταίας ενημέρωσης
+        tacho.calculateRpm();
+        tacho.updateDisplay();
+        tacho.lastDisplayTime = localCurrentLoopTime; // Αποθήκευση χρόνου τελευταίας ενημέρωσης
     }
 }
